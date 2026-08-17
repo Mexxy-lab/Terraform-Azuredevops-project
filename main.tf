035b0f55916682b6c93557fae1757efc95a9aa04
Emeka Umejiofor <pumej12@pumej.onmicrosoft.com>
2026-08-17 19:55:49 -0400

    added files for main.tf to use version 5.0.0

--- main.tf
+++ main.tf
@@ -1,3 +1,101 @@
+# resource "azurerm_resource_group" "rg" {
+#   name     = "${var.prefix}-resources"
+#   location = var.location
+# }
+
+# resource "azurerm_storage_account" "tfstate_container" {
+#   name                     = "pumejtfstate"
+#   resource_group_name      = azurerm_resource_group.rg.name
+#   location                 = azurerm_resource_group.rg.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+
+#   allow_nested_items_to_be_public = false
+
+#   blob_properties {
+#     versioning_enabled = true
+#   }
+
+#   tags = {
+#     environment = "prod"
+#   }
+# }
+
+# resource "azurerm_storage_container" "tfstate_container" {
+#   name                  = "prod-tfstate"
+#   storage_account_name  = azurerm_storage_account.tfstate_container.name
+#   container_access_type = "private"
+# }
+
+# resource "azurerm_resource_group" "example" {
+#   name     = "${var.prefix}-rg"
+#   location = var.location
+# }
+
+# resource "azurerm_virtual_network" "main" {
+#   name                = "${var.prefix}-network"
+#   address_space       = ["10.0.0.0/16"]
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
+# }
+
+# resource "azurerm_subnet" "internal" {
+#   name                 = "internal"
+#   resource_group_name  = azurerm_resource_group.example.name
+#   virtual_network_name = azurerm_virtual_network.main.name
+#   address_prefixes     = ["10.0.2.0/24"]
+# }
+
+# resource "azurerm_network_interface" "main" {
+#   name                = "${var.prefix}-nic"
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
+
+#   ip_configuration {
+#     name                          = "testconfiguration1"
+#     subnet_id                     = azurerm_subnet.internal.id
+#     private_ip_address_allocation = "Dynamic"
+#   }
+# }
+
+# resource "azurerm_virtual_machine" "main" {
+#   name                  = "${var.prefix}-vm"
+#   location              = azurerm_resource_group.example.location
+#   resource_group_name   = azurerm_resource_group.example.name
+#   network_interface_ids = [azurerm_network_interface.main.id]
+#   vm_size               = "Standard_DS1_v2"
+
+#   # Uncomment this line to delete the OS disk automatically when deleting the VM
+#   # delete_os_disk_on_termination = true
+
+#   # Uncomment this line to delete the data disks automatically when deleting the VM
+#   # delete_data_disks_on_termination = true
+
+#   storage_image_reference {
+#     publisher = "Canonical"
+#     offer     = "0001-com-ubuntu-server-jammy"
+#     sku       = "22_04-lts"
+#     version   = "latest"
+#   }
+#   storage_os_disk {
+#     name              = "myosdisk1"
+#     caching           = "ReadWrite"
+#     create_option     = "FromImage"
+#     managed_disk_type = "Standard_LRS"
+#   }
+#   os_profile {
+#     computer_name  = "hostname"
+#     admin_username = "testadmin"
+#     admin_password = "Password1234!"
+#   }
+#   os_profile_linux_config {
+#     disable_password_authentication = false
+#   }
+#   tags = {
+#     environment = "staging"
+#   }
+# }
+
 resource "azurerm_resource_group" "rg" {
   name     = "${var.prefix}-resources"
   location = var.location
@@ -23,7 +121,7 @@ resource "azurerm_storage_account" "tfstate_container" {
 
 resource "azurerm_storage_container" "tfstate_container" {
   name                  = "prod-tfstate"
-  storage_account_name  = azurerm_storage_account.tfstate_container.name
+  storage_account_id    = azurerm_storage_account.tfstate_container.id
   container_access_type = "private"
 }
 
@@ -65,10 +163,9 @@ resource "azurerm_virtual_machine" "main" {
   network_interface_ids = [azurerm_network_interface.main.id]
   vm_size               = "Standard_DS1_v2"
 
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
-
-  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # Uncomment these if you want Terraform to delete the disks
+  # when the VM is destroyed.
+  # delete_os_disk_on_termination   = true
   # delete_data_disks_on_termination = true
 
   storage_image_reference {
@@ -77,20 +174,24 @@ resource "azurerm_virtual_machine" "main" {
     sku       = "22_04-lts"
     version   = "latest"
   }
+
   storage_os_disk {
     name              = "myosdisk1"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
+
   os_profile {
     computer_name  = "hostname"
     admin_username = "testadmin"
     admin_password = "Password1234!"
   }
+
   os_profile_linux_config {
     disable_password_authentication = false
   }
+
   tags = {
     environment = "staging"
   }
